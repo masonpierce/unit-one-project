3b54ea949675ced84fb0d9e50d1046305dbcfe69
Mason Pierce <masonpierce@Masons-MacBook-Air.local>
2019-08-07 14:28:08 -0500

    deploy

--- code/unit-one-project/js/main.js
+++ code/unit-one-project/js/main.js
@@ -1,8 +1,6 @@
 /*----- constants -----*/
 var suits = ['s', 'c', 'd', 'h'];
 var ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
-var deck;
-var masterDeck;
 
 const backCard = {
   blue: "images/backs/blue.svg",
